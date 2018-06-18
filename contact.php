4a3377c94957b529ca0e0705160528f876bd4b31
Philipp Preuss <philipp.arian@gmail.com>
2018-06-18 11:55:15 +0200

    minor changes

--- contact.php
+++ contact.php
@@ -2,10 +2,10 @@
 
 // configure
 $from = 'Demo contact form <demo@domain.com>';
-$sendTo = 'Demo contact form <demo@domain.com>'; // Add Your Email
+$sendTo = 'Demo contact form <philipp.arian@gmail.com>'; // Add Your Email
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+$okMessage = 'Thanks for reaching out, I will get back to you soon!';
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
 // let's do the sending
